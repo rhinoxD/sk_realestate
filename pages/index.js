898579c6157d3751c852394e1ec5284552c9c132
unknown <coolshiv0721@gmail.com>
2022-07-11 10:38:48 +0530

    Edited button and created fetchApi

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ const Banner = ({
       <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>
         {desc1} <br /> {desc2}
       </Text>
-      <Button fontSize='xl' bg='blue.300' color='white'>
+      <Button fontSize='xl'>
         <Link href={linkName}>{buttonText}</Link>
       </Button>
     </Box>
@@ -33,7 +33,7 @@ const Banner = ({
 
 export default function Home() {
   return (
-    <div>
+    <Box>
       <Banner
         purpose='RENT A HOME'
         title1='Rental Homes for'
@@ -44,6 +44,9 @@ export default function Home() {
         linkName='/search?purpose=for-rent'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
       />
+      <Flex>
+        {/* Fetch the properties and map over them... */}
+      </Flex>
       <Banner
         purpose='BUY A HOME'
         title1=' Find, Buy & Own Your'
@@ -54,6 +57,6 @@ export default function Home() {
         linkName='/search?purpose=for-sale'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
       />
-    </div>
+    </Box>
   );
 }
