e893d226d761628cfa6a87b32298c8925502eb18
unknown <coolshiv0721@gmail.com>
2022-09-30 18:39:05 +0530

    Created dumped filterData from gh and updated SearchFilters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,13 +1,32 @@
 import { useEffect, useState } from 'react';
-import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button, filter } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
 
+import {filterData, getFilterValues} from '../utils/filterData';
+
 const SearchFilters = () => {
+  const [filters, setFilters] = useState(filterData);
+  const searchProperties = (filterValues) => {
+
+  }
   return (
-    <div>SearchFilters</div>
+    <Flex bg='gray.100' justifyContent='center' flexWrap='wrap'>
+      {filters.map((filter) => (
+        <Box key={filter.queryname}>
+          <Select 
+            placeholder={filter.placeholder}
+            w='fit conten'
+            p='2'
+            onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}
+            >
+
+          </Select>
+        </Box>
+      ))}
+    </Flex>
   )
 }
 
-export default SearchFilters
+export default SearchFilters;
