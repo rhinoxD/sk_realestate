b5df8a496db38fe94efe27f3b427c84bc7b8eaab
unknown <coolshiv0721@gmail.com>
2022-09-30 18:28:05 +0530

    Created serverSideProps in search page

--- pages/search.js
+++ pages/search.js
@@ -6,10 +6,10 @@ import { BsFilter } from 'react-icons/bs';
 
 import Property from '../components/Property';
 import SearchFilters from '../components/SearchFilters';
-
 import noresult from '../assets/images/noresult.svg';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
 
-const Search = () => {
+const Search = ({ properties }) => {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
   return (
@@ -34,11 +34,11 @@ const Search = () => {
         Properties {router.query.purpose}
       </Text>
       <Flex flexWrap='wrap'>
-        {[].map((property) => (
+        {properties.map((property) => (
           <Property property={property} key={property.id} />
         ))}
       </Flex>
-      {[].length === 0 && (
+      {properties.length === 0 && (
         <Flex
           justifyContent='center'
           alignItems='center'
@@ -55,5 +55,28 @@ const Search = () => {
     </Box>
   );
 };
+
+export async function getServerSideProps({ query }) {
+  const purpose = query.purpose || 'for-rent';
+  const rentFrequency = query.rentFrequency || 'yearly';
+  const minPrice = query.minPrice || '0';
+  const maxPrice = query.maxPrice || '1000000';
+  const roomsMin = query.roomsMin || '0';
+  const bathsMin = query.bathsMin || '0';
+  const sort = query.sort || 'price-desc';
+  const areaMax = query.areaMax || '35000';
+  const locationExternalIDs = query.locationExternalIDs || '5002';
+  const categoryExternalID = query.categoryExternalID || '4';
+
+  const data = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`
+  );
+
+  return {
+    props: {
+      properties: data?.hits,
+    },
+  };
+}
 
 export default Search;
