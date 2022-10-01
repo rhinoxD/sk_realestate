e6cf1fe3ceddcf603acb0e2b04584af5ee26eeeb
unknown <coolshiv0721@gmail.com>
2022-10-01 11:57:52 +0530

    Implemented search by filters functionality in SearchFilters

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -8,8 +8,16 @@ import {filterData, getFilterValues} from '../utils/filterData';
 
 const SearchFilters = () => {
   const [filters, setFilters] = useState(filterData);
-  const searchProperties = (filterValues) => {
+  const router = useRouter();
 
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const {query} = router;
+    const values = getFilterValues(filterValues);
+    values.forEach((item) => {
+      query[item.name] = item.value 
+    })
+    router.push({pathname: path, query})
   }
   return (
     <Flex bg='gray.100' justifyContent='center' flexWrap='wrap'>
@@ -17,11 +25,13 @@ const SearchFilters = () => {
         <Box key={filter.queryname}>
           <Select 
             placeholder={filter.placeholder}
-            w='fit conten'
+            w='fit content'
             p='2'
             onChange={(e) => searchProperties({[filter.queryName]: e.target.value})}
             >
-
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>{item.name}</option>
+            ))}
           </Select>
         </Box>
       ))}
